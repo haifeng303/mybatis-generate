b3d00f1b41e72974fda8d8cbc7efa2d23a4ea9df
ranhaifeng <ranhaifeng@prosysoft.com>
2020-07-07 14:38:01 +0800

    初始化项目

---
+++ src/main/java/com/hf/core/BaseController.java
@@ -0,0 +1,12 @@
+package com.hf.core;
+
+import java.io.Serializable;
+
+/**
+ * @PACKAGE_NAME: com.hf.core
+ * @author: rhf
+ * @description:
+ * @DATE: 2020/7/7
+ **/
+public class BaseController<E extends Serializable> {
+}
